97eb3a5dfe73eeb9fe4573148df89276e18b36ca
lgkinter <lgkinter@yahoo.com>
2016-07-15 23:27:10 -0400

    Add limit to number of guesses

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 set :number, rand(100)
+@@remaining_guesses = 5
+@@correct_guess = settings.number
 
 def check_guess(guess)
   if guess > settings.number + 5
@@ -17,8 +19,25 @@ def check_guess(guess)
   end
 end
 
+def game_over?(message)
+  if @@remaining_guesses == 0 || message =~ /right/
+    @@correct_guess = settings.number
+    settings.number = rand(100)
+    @@remaining_guesses = 5
+    return true
+  else
+    return false
+  end
+end
+
 get '/' do
   guess = params['guess'].to_i
-  message = check_guess(guess)
-  erb :index, :locals => {:number => settings.number, :message => message}
+  if params['guess']
+    @@remaining_guesses -= 1
+    message = check_guess(guess)
+  else
+    message = "Welcome! We've selected a random secret number from 0 to 100. What's your guess?"
+  end
+  game_over = game_over?(message)
+  erb :index, :locals => {:number => @@correct_guess, :message => message, :game_over => game_over, :guess_count => @@remaining_guesses}
 end
